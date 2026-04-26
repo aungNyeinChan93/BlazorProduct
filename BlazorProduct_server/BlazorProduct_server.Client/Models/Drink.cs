3e7bfee9bd1d9d644f101d5ed13239f534e89d47
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-04-26 16:40:13 +0630

    Feat: cascading value

---
+++ BlazorProduct_server/BlazorProduct_server.Client/Models/Drink.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorProduct_server.Client.Models
+{
+    public class Drink
+    {
+        public int DrinkId { get; set; }
+        public string? Name { get; set; }
+
+        public string? Type { get; set; }
+    }
+}
