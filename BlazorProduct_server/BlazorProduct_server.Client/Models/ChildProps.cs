16b0d4084c900c1d4e9edb2022ffeb87148e9622
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-04-26 16:18:56 +0630

    Feat: parent to child relation and prop and model dralling

---
+++ BlazorProduct_server/BlazorProduct_server.Client/Models/ChildProps.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorProduct_server.Client.Models
+{
+    public class ChildProps
+    {
+        public int ChildPropsId { get; set; }
+        public string Message { get; set; }
+
+        public Test test { get; set; }
+    }
+}
