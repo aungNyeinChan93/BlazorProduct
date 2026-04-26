2c4d08574ab8b43ace99032401ff2cdd64e5a840
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-04-26 12:01:27 +0630

    Feat: test one

---
+++ BlazorProduct_server/BlazorProduct_server.Client/Models/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorProduct_server.Client.Models
+{
+    public class Test
+    {
+        public int TestId { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool Status { get; set; }
+
+        public IEnumerable<TestProps> TestPropetries { get; set; }
+    }
+}
---
+++ BlazorProduct_server/BlazorProduct_server.Client/Models/TestProps.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorProduct_server.Client.Models
+{
+    public class TestProps
+    {
+        public int TestPropsId { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
